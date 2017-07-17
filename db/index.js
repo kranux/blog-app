595df25d8c07968047248d1701fec55c31aa9298
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2017-07-17 19:58:57 +0300

    Lint all the server things;

--- db/index.js
+++ db/index.js
@@ -1,25 +1,23 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
 
-module.exports.initialize = function() {
-  db.serialize(function() {
-    db.run("CREATE TABLE posts (title TEXT, content TEXT, author NUMBER, created DATE, modified DATE)");
+module.exports.initialize = () => {
+	db.serialize(() => {
+		db.run('CREATE TABLE posts (title TEXT, content TEXT, author NUMBER, created DATE, modified DATE)');
 
-    var stmt = db.prepare("INSERT INTO posts VALUES (?, ?, ?, ?, ?)");
-    for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i, "ipsum" + i, 1, new Date(), new Date());
-    }
-    stmt.finalize();
-  });
+		var stmt = db.prepare('INSERT INTO posts VALUES (?, ?, ?, ?, ?)');
+		for (var i = 0; i < 10; i++) {
+			stmt.run(`Ipsum${i}`, `ipsum${i}`, 1, new Date(), new Date());
+		}
+		stmt.finalize();
+	});
 };
 
-module.exports.getUsers = function() {
-  return new Promise((resolve, reject)=>{
-    db.all("SELECT rowid AS id, title, content, author, created, modified FROM posts", (err, data) => {
-      if (err) {
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-};
+module.exports.getUsers = () => new Promise((resolve, reject) => {
+	db.all('SELECT rowid AS id, title, content, author, created, modified FROM posts', (err, data) => {
+		if (err) {
+			reject(err);
+		}
+		resolve(data);
+	});
+});
--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,12 @@ var router = express.Router();
 var db = require('../db');
 
 /* GET posts listing. */
-router.get('/', async (req, res, next) => {
-  try {
-    res.send(await db.getUsers());
-  } catch (ex) {
-    res.send('An error has occoured');
-  }
+router.get('/', async (req, res) => {
+	try {
+		res.send(await db.getUsers());
+	} catch (ex) {
+		res.send('An error has occoured');
+	}
 });
 
 module.exports = router;
