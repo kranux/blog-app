04b9f9632543c35570dfc9ef61bb3b54d6003303
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2017-07-21 18:28:44 +0300

    Allow to re-login;

--- client/app/services/auth.js
+++ client/app/services/auth.js
@@ -3,11 +3,14 @@ import Ember from 'ember';
 export default Ember.Service.extend({
 	user: null,
 
+	store: Ember.inject.service('store'),
+
 	setUser: function(user) {
 		this.set('user', user);
 	},
 
 	logout: function() {
 		this.set('user', null);
+		this.get('store').unloadAll('user');
 	}
 });
