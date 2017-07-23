0cacd8f04831fcb626bf0a0ee9c7fbc402c075d2
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2017-07-23 10:44:38 +0300

    Remove unneded package;

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const session = require('express-session');
-const flash = require('connect-flash');
 const db = require('./db');
 const sessionstore = require('sessionstore');
 const app = express();
